6ef7c2d1c35f265828948668518f296cb699a0fe
Simon <wsj_simon@163.com>
2022-08-10 10:08:04 +0800

    feat(ch03): update practice

--- part01/ch03/practice_ch03_04.cpp
+++ part01/ch03/practice_ch03_04.cpp
@@ -37,9 +37,80 @@ int p3_23 () {
   return 0;
 }
 
+int p3_24 () {
+  vector<int> ivec{123, 234, 456, 567, 678, 789, 890, 901};
+  vector<int> reverseSumVec;
+  auto start = ivec.begin();
+  while (start != ivec.end() - 1) {
+    cout << *start + *(start + 1) << ", ";
+    start++;
+  }
+
+  cout << endl;
+
+  start = ivec.begin();
+  auto end = ivec.end() - 1;
+
+  while (start < end) {
+    cout << *start + *end << ", ";
+    start++;
+    end--;
+  }
+
+  cout << endl;
+  return 0;
+}
+
+int p3_25 () {
+  vector<unsigned> scores(11, 0);
+  vector<unsigned> grade{13, 42, 56, 68, 75, 77, 76, 85, 95, 100, 7};
+  auto start = grade.begin();
+  while (start != grade.end()) {
+    if (*start <= 100) {
+      int idx = *start / 10;
+      *(scores.begin() + idx) += 1;
+    }
+    start++;
+  }
+
+  return 0;
+}
+
+int p3_26 () {
+  // 迭代器之间只能 - ，不能 +，相减后的值，可以相加
+  // mid = beg + （end - beg） / 2
+  // mid = （beg + end） / 2
+  vector<unsigned> scores{12, 23, 45, 56, 58, 68, 79, 88, 89, 90, 102};
+  auto beg = scores.begin();
+  auto end = scores.end() - 1;
+
+  int findInt = 45;
+  vector<unsigned>::iterator ans;
+
+  while (beg <= end) {
+    auto mid = beg + (end - beg) / 2;
+    cout << *mid << endl;
+    if (findInt < *mid) {
+      end = mid - 1;
+    } else if (findInt > *mid) {
+      beg = mid + 1;
+    } else {
+      ans = mid;
+      break;
+    }
+  }
+
+  cout << *ans << endl;
+
+  return 0;
+}
+
 int main () {
   p3_21_22();
   p3_23();
+  p3_24();
+  p3_25();
+  p3_26();
 
   return 0;
 }
