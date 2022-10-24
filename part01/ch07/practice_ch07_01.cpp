da24759ded02d714870a119234ce49c320e5ee3f
Simon <wsj_simon@163.com>
2022-10-24 10:52:18 +0800

    feat(ch07): update 7.1 practice

--- part01/ch07/practice_ch07_01.cpp
+++ part01/ch07/practice_ch07_01.cpp
@@ -83,9 +83,114 @@ int p7_02_03 () {
   return 0;
 }
 
+/**
+ * 练习7.4：编写一个名为Person的类，使其表示人员的姓名和住址。
+ * 使用string对象存放这些元素，接下来的练习将不断充实这个类的其他特征。
+ *
+ * 练习7.5：在你的Person类中提供一些操作使其能够返回姓名和住址。这些函数是否应该是const的呢？解释原因。
+ */
+struct Person {
+  string name;
+  string address;
+
+public:
+  auto getName() const -> string const & { return name; };
+  auto getAddress() const -> string const & { return address; };
+};
+
+void p7_04_05 () {
+}
+
+/**
+ * 练习7.6：对于函数add、read和print，定义你自己的版本。
+ *
+ * 练习7.7：使用这些新函数重写7.1.2节（第233页）练习中的交易处理程序。
+ */
+std::istream &read(std::istream &is, Sales_data &item) {
+  double price = 0;
+  is >> item.bookNo >> item.units_sold >> price;
+  item.revenue = price * item.units_sold;
+  return is;
+}
+std::ostream &print(std::ostream &os, const Sales_data &item) {
+  os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+  return os;
+}
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+  Sales_data sum = lhs;
+  sum.combine(rhs);
+  return sum;
+}
+
+int p7_06_07 () {
+  Sales_data total;
+
+  if (read(cin, total)) {
+    Sales_data trans;
+
+    while (read(cin, trans)) {
+      if (total.isbn() == trans.isbn()) {
+        total.combine(trans);
+      } else {
+        print(cout, total) << std::endl;
+        total = trans;
+      }
+    }
+    print(cout, total) << std::endl;
+  } else {
+    std::cerr << "No data?" << endl;
+    return -1;
+  }
+  return 0;
+}
+
+/**
+ * 练习7.8：为什么read函数将其Sales_data参数定义成普通的引用，而print将其参数定义成常量引用？
+ */
+void p7_08 () {
+  // read 要修改
+  // print 只读
+}
+
+/**
+ * 练习7.9：对于7.1.2节（第233页）练习中的代码，添加读取和打印Person对象的操作。
+ */
+std::istream &read(std::istream &is, Person &item) {
+  is >> item.name >> item.address;
+  return is;
+}
+std::ostream &print(std::ostream &os, const Person &item) {
+  os << item.getName() << " " << item.getAddress();
+  return os;
+}
+void p7_09 () {
+  // read 要修改
+  // print 只读
+  Person someone;
+  if (read(cin, someone)) {
+    print(cout, someone);
+  }
+}
+
+
+/**
+ * 练习7.10：在下面这条if语句中，条件部分的作用是什么？
+ */
+// if (read(read(cin, data1), data2))
+void p7_10 () {
+  // 相当于 cin >> data1 >> data2
+}
+
+
+
 int main () {
   p7_01();
   p7_02_03();
+  p7_04_05();
+  p7_06_07();
+  p7_08();
+  p7_09();
+  p7_10();
 
   return 0;
 }
