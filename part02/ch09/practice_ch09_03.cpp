87791f5cb0854400907be7ea7e356d500ba469e0
Simon <wsj_simon@163.com>
2023-01-03 10:55:03 +0800

    feat(ch09): update 9.3 practice

--- part02/ch09/practice_ch09_03.cpp
+++ part02/ch09/practice_ch09_03.cpp
@@ -90,11 +90,50 @@ void p9_21 () {
   // 每次insert 返回被往前插入的 iter，该 iter 是就是 begin，饭后赋值给 iter，再继续循环。
 }
 
+/**
+ * 练习9.22：假定iv是一个int的vector，下面的程序存在什么错误？你将如何修改？
+ */
+void p9_22 () {
+  vector<int> iv = {1,2,3,4,5,6,7};
+  int some_val = 3;
+  vector<int>::iterator iter = iv.begin(),
+    mid = iv.begin() + iv.size() / 2;
+
+  while (iter != mid) {
+    if (*iter == some_val) {
+      iv.insert(iter, 2 * some_val);
+      ++iter;
+    }
+    ++iter;
+  }
+}
+
+/**
+ * 练习9.23：在本节第一个程序（第309页）中，若c.size（）为1，则val、val2、val3和val4的值会是什么？
+ */
+void p9_23 () {
+ // 与第一个值相同
+}
+
+/**
+ * 练习9.24：编写程序，分别使用at、下标运算符、front和begin提取一个vector中的第一个元素。在一个空vector上测试你的程序。
+ */
+void p9_24 () {
+  vector<int> vi;
+  cout << vi.at(0) << endl;
+  cout << vi[0] << endl;
+  cout << vi.front() << endl;
+  cout << *(vi.begin()) << endl;
+}
+
 int main (int argc, char **argv) {
-  p9_18();
-  p9_19();
-  p9_20();
-  p9_21();
+  // p9_18();
+  // p9_19();
+  // p9_20();
+  // p9_21();
+  // p9_22();
+  // p9_23();
+  p9_24();
 
   return 0;
 }
