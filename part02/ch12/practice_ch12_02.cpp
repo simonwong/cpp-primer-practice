26b570cb343b23d40c9b6f1a107994da96553305
Simon <wsj_simon@163.com>
2023-04-17 10:13:16 +0800

    feat(ch12): update 12.2 practice

---
+++ part02/ch12/practice_ch12_02.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <map>
+#include <stdexcept>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+/**
+ * 练习12.23：编写一个程序，连接两个字符串字面常量，将结果保存在一个动态分配的char数组中。
+ * 重写这个程序，连接两个标准库string对象。
+ */
+char* mergeChar (char a, char b) {
+  auto p = new char[2]{ a, b };
+
+  return p;
+}
+string* mergeString (string a, string b) {
+  auto p = new string[a.size() + b.size()]{ a, b };
+
+  return p;
+}
+void p12_23 () {
+}
+
+/**
+ * 练习12.24：编写一个程序，从标准输入读取一个字符串，存入一个动态分配的字符数组中。
+ * 描述你的程序如何处理变长输入。测试你的程序，输入一个超出你分配的数组长度的字符串。
+ */
+void p12_24 () {
+  auto *p = new string[10];
+  string word;
+  int i = 0;
+  while (cin >> word) {
+    *(p + i) = word;
+    ++i;
+  }
+
+  delete [] p;
+}
+
+/**
+ * 练习12.25：给定下面的new表达式，你应该如何释放pa？
+ */
+void p12_25 () {
+  int *pa = new int[10];
+
+  delete [] pa;
+}
+
+int main () {
+  p12_23();
+  p12_24();
+  p12_25();
+
+  return 0;
+}
