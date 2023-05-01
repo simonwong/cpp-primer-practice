df3ab575ec7e5dfef0e04e56e967bf4420ed2bb7
Simon <wsj_simon@163.com>
2023-05-01 14:14:51 +0800

    feat(ch13): update 13.1 practice

--- part03/ch13/practice_ch13_01.cpp
+++ part03/ch13/practice_ch13_01.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::vector;
 
 /**
  * 练习13.1：拷贝构造函数是什么？什么时候使用它？
@@ -93,6 +95,89 @@ private:
 void p13_08 () {
 }
 
+/**
+ * 练习13.9：析构函数是什么？合成析构函数完成什么工作？什么时候会生成合成析构函数？
+ */
+void p13_09 () {
+  // 析构函数释放对象的资源，并销毁对象的非 static 数据成员。
+  // 未定义自己的析构函数，编译器会为它定义一个析构函数。
+}
+
+/**
+ * 练习13.10：当一个StrBlob对象销毁时会发生什么？一个StrBlobPtr对象销毁时呢？
+ */
+void p13_10 () {
+  // use_count 减去 1
+  // 动态对象不会被释放
+}
+
+/**
+ * 练习13.11：为前面练习中的HasPtr类添加一个析构函数。
+ */
+class HasPtr3 {
+public:
+  HasPtr3(const string& s = string()) : ps(new string(s)), i(0) {}
+  HasPtr3(const HasPtr3& hp) : ps(new string(*hp.ps)), i(hp.i) {};
+  HasPtr3& operator=(const HasPtr3&);
+  ~HasPtr3() { };
+private:
+  string *ps;
+  int i;
+};
+void p13_11 () {
+}
+
+/**
+ * 练习13.12：在下面的代码片段中会发生几次析构函数调用？
+ */
+void p13_12 () {
+  // bool fcn(const Sales_data *trans, Sales_data accum) {
+  //   Sales_data item1(*trans), item2(accum);
+  //   return item1.isbn() != item2.isbn(); // item1、item2、accum 会被调用
+  // }
+}
+
+/**
+ * 练习13.13：理解拷贝控制成员和构造函数的一个好方法是定义一个简单的类，
+ * 为该类定义这些成员，每个成员都打印出自己的名字：
+ *
+ * 给X添加拷贝赋值运算符和析构函数，并编写一个程序以不同方式使用X的对象：
+ * 将它们作为非引用和引用参数传递；
+ * 动态分配它们；
+ * 将它们存放于容器中；
+ * 诸如此类。
+ * 观察程序的输出，直到你确认理解了什么时候会使用拷贝控制成员，以及为什么会使用它们。
+ * 当你观察程序输出时，记住编译器可以略过对拷贝构造函数的调用。
+ */
+struct X
+{
+  X() {
+    cout << "X()" << endl;
+  }
+  X(const X&) {
+    cout << "X(const X&)" << endl;
+  }
+  X& operator=(const X&) {
+    cout << "X& operator=(const X&)" << endl;
+    return *this;
+  }
+  ~X() {
+    cout << "~X()" << endl;
+  }
+};
+
+void p13_13 () {
+  X x1;
+  X x2 = x1;
+
+  X *px = new X;
+
+  vector<X> vec;
+  vec.push_back(*px);
+
+  delete px;
+}
+
 int main () {
   // p13_01();
   // p13_02();
@@ -102,6 +187,11 @@ int main () {
   // p13_06();
   // p13_07();
   // p13_08();
+  // p13_09();
+  // p13_10();
+  // p13_11();
+  // p13_12();
+  p13_13();
 
   return 0;
 }
