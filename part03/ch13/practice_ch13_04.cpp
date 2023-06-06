ebe17138fb9c69b3504cab0ac871ec1027cd7e09
Simon <wsj_simon@163.com>
2023-06-06 09:29:13 +0800

    feat: update 13.4 practice

--- part03/ch13/practice_ch13_04.cpp
+++ part03/ch13/practice_ch13_04.cpp
@@ -142,12 +142,113 @@ void p13_43 () {
   // 写 for_each + lambda 表达式更加简洁
 }
 
+/**
+ * 练习13.44：编写标准库string类的简化版本，命名为String。
+ * 你的类应该至少有一个默认构造函数和一个接受C风格字符串指针参数的构造函数。
+ * 使用allocator为你的String类分配所需内存。
+ */
+
+class String {
+public:
+  String(): elements(nullptr), first_free(nullptr), cap(nullptr) {}
+
+  String(std::initializer_list<char> listStr): elements(nullptr), first_free(nullptr), cap(nullptr) {
+    auto newData = alloc_n_copy(listStr.begin(), listStr.end());
+    elements = newData.first;
+    first_free = newData.second;
+  }
+
+  String(const String &s) {
+    auto newData = alloc_n_copy(s.begin(), s.end());
+    elements = newData.first;
+    first_free = newData.second;
+  }
+
+  String &operator=(const String &rhs) {
+    auto data = alloc_n_copy(rhs.begin(), rhs.end());
+    free();
+    elements = data.first;
+    first_free = cap = data.second;
+    return *this;
+  }
+
+  ~String() {
+    free();
+  }
+
+  void push_back(const char &s) {
+    chk_n_alloc();
+    alloc.construct(first_free++, s);
+  };
+
+  size_t size() const {
+    return first_free - elements;
+  }
+
+  size_t capacity () const {
+    return cap - elements;
+  }
+
+  char *begin() const {
+    return elements;
+  }
+
+  char *end() const {
+    return first_free;
+  }
+
+private:
+  static std::allocator<char> alloc;
+
+  void chk_n_alloc () {
+    if (size() == capacity()) {
+      reallocate();
+    }
+  }
+
+  std::pair<char*, char*> alloc_n_copy(const char*b, const char*e) {
+    auto data = alloc.allocate(e - b);
+    return {
+      data,
+      std::uninitialized_copy(b, e, data),
+    };
+  }
+  void free() {
+    if (elements) {
+      for (auto p = first_free; p != elements; ) {
+        alloc.destroy(--p);
+      }
+      alloc.deallocate(elements, cap - elements);
+    }
+  }
+  void reallocate() {
+    auto newCapacity = size() ? 2 * size() : 1;
+    auto newData = alloc.allocate(newCapacity);
+
+    auto dest = newData;
+    auto elem = elements;
+    for (size_t i = 0; i != size(); ++i) {
+      alloc.construct(dest++, std::move(*elem++));
+    }
+    free();
+    elements = newData;
+    first_free = dest;
+    cap = elements + newCapacity;
+  };
+  char *elements;
+  char *first_free;
+  char *cap;
+};
+void p13_44 () {
+}
+
 int main () {
   // p13_39();
   // p13_40();
   // p13_41();
   // p13_42();
   // p13_43();
+  // p13_44();
 
   return 0;
 }
